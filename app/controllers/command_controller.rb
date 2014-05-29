98c2e01b886191c7ef6cc9a410d2c9d4c7fff608
Genevieve Parkes & Nick Johnstone <pair+GenevieveBelle.Widdershin@devbootcamp.com>
2014-05-30 11:46:49 +1200

    Added split_input method, loot and go commands + 3 tests

--- app/controllers/command_controller.rb
+++ app/controllers/command_controller.rb
@@ -6,6 +6,18 @@ class CommandController
   end
 
   def route_command(command)
-    @game_controller.look
+    command, argument = split_input(command)
+    case command
+      when 'look'
+        game_controller.look
+      when 'loot'
+        game_controller.loot
+      when 'go'
+        game_controller.go(argument.to_sym)
+    end
+  end
+
+  def split_input(command)
+    command.split(' ')
   end
 end
--- spec/command_controller_spec.rb
+++ spec/command_controller_spec.rb
@@ -9,8 +9,27 @@ describe CommandController do
   it 'should have a game controller' do
     expect(@controller.game_controller).to be_a(GameController)
   end
-  it 'should route the look command' do
-    @controller.game_controller.should_receive(:look)
-    @controller.route_command('look')
+
+  describe 'split_input' do
+    it 'will split input into command and argument' do
+      expect(@controller.split_input('go east')).to eq ['go', 'east']
+    end
+  end
+
+  describe 'commands' do
+    it 'should route the look command' do
+      @controller.game_controller.should_receive(:look)
+      @controller.route_command('look')
+    end
+
+    it 'should route the loot command' do
+      @controller.game_controller.should_receive(:loot)
+      @controller.route_command('loot')
+    end
+
+    it 'should route the go command' do
+      @controller.game_controller.should_receive(:go).with(:east)
+      @controller.route_command('go east')
+    end
   end
 end
