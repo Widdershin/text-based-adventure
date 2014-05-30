093f7f12973cb48bd10e32b2c50cf6d1c47c0170
Genevieve Parkes & Nick Johnstone <pair+GenevieveBelle.Widdershin@devbootcamp.com>
2014-05-30 17:55:29 +1200

    Made things work

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,8 +8,9 @@ class GameController
 
     # TODO: Make rooms not hardcoded
 
-    @current_room = Room.new(description: 'You are in a test room. The walls are a faded white, and there is no exit you can see. Cry.', gems: 9,  directions: {north:nil,south:nil})
-    @current_room.set_enemy(Goblin.new)
+    # @current_room = Room.new(description: 'You are in a test room. The walls are a faded white, and there is no exit you can see. Cry.', gems: 9,  directions: {north:nil,south:nil})
+    @current_room = RoomLoader.load_rooms
+    # @current_room.set_enemy(Goblin.new)
 
     @room_to_move_to = current_room
 
